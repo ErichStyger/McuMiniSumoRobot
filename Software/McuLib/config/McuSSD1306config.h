03ad78a0ce5038b72351f8abfa89606d6b98d3be
Erich Styger <erich.styger@hslu.ch>
2018-01-28 12:24:57 +0100

    updated to latest McuOnEclipseLibrary

--- Software/McuLib/config/McuSSD1306config.h
+++ Software/McuLib/config/McuSSD1306config.h
@@ -37,6 +37,11 @@
     /*!< I2C 7bit (unshifted) device address, usually 0x3C (0b11'1100) or 0x3D (0b11'1101) */
 #endif
 
+#ifndef McuSSD1306_CONFIG_SSD1306_DRIVER_TYPE
+  #define McuSSD1306_CONFIG_SSD1306_DRIVER_TYPE        (1306)
+    /*!< Either 1306 for SSD1306 or 1106 for SH1106 */
+#endif
+
 #ifndef McuSSD1306_CONFIG_SSD1306_I2C_DELAY_US
   #define McuSSD1306_CONFIG_SSD1306_I2C_DELAY_US    (0)
     /*!< I2C transaction delay in us */
@@ -71,6 +76,11 @@
 #ifndef McuSSD1306_CONFIG_INIT_DELAY_MS
   #define McuSSD1306_CONFIG_INIT_DELAY_MS (50)
   /*!< Additional delay (milliseconds) in Init(). Use zero for no delay. */
+#endif
+
+#ifndef McuSSD1306_CONFIG_USE_I2C_BLOCK_TRANSFER
+  #define McuSSD1306_CONFIG_USE_I2C_BLOCK_TRANSFER (1)
+  /*!< 1: use I2C Block transfer for better performance. 0: disabled, e.g. for slower displays */
 #endif
 
 #ifndef McuSSD1306_CONFIG_USE_RAM_BUFFER
